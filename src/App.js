4b98feea7d2b668dce74b578d29a104611a4f335
Ridho Gumelar Bagaskara <47409331+ridhliver@users.noreply.github.com>
2020-06-15 19:52:18 +0700

    reactjs

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import {
+  Route,
+  NavLink,
+  HashRouter,
+  BrowserRouter
+} from "react-router-dom";
+import HorizontalLinearStepper from "./app/pages/Register";
+ 
+class Main extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <div className="content">
+            <Route path="/register/store" component={HorizontalLinearStepper}/>
+          </div>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
-
-export default App;
+ 
+export default Main;
